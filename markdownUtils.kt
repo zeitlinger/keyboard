6cc42caf47b8a89ad2f45519d8049aeec9031ae1
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-11-12 07:59:13 +0100

    symbol combos

--- markdownUtils.kt
+++ markdownUtils.kt
@@ -4,26 +4,28 @@ import java.io.File
 const val blocked = "XX"
 
 fun readTables(config: File): Tables =
-        config.readText().split("\n\\s*\n".toRegex()).filter { it.startsWith("|") }.map { tableLines ->
-            val entries = tableLines.split("\n").filterIndexed { index, line ->
-                index != 1               // below header
-                        && line.isNotBlank() // last block can contain empty line at end
-            }
-            entries.map { tableLine ->
-                tableLine.split("|").drop(1) // initial |
-                        .dropLast(1) // last |
-                        .map { it.trim() }
-            }
+    config.readText().split("\n\\s*\n".toRegex()).filter { it.startsWith("|") }.map { tableLines ->
+        val entries = tableLines.split("\n").filterIndexed { index, line ->
+            line.isNotBlank() // last block can contain empty line at end
+        }
+        entries
+            .filter { !it.contains("--") } // visual divider row and below header
+            .map { tableLine ->
+            tableLine.split("|")
+                .drop(1) // initial |
+                .dropLast(1) // last |
+                .map { it.trim() }
+        }
 
-        }.let { Tables(it) }
+    }.let { Tables(it) }
 
 
 const val inputBlocked = "❌"
 
 data class Symbols(val mapping: Map<String, String>) {
     fun replace(key: String): String = mapping.getOrDefault(key, key)
-            .let { it.ifBlank { blocked } }
-            .let { if (it == inputBlocked) blocked else it }
+        .let { it.ifBlank { blocked } }
+        .let { if (it == inputBlocked) blocked else it }
 }
 
 typealias Table = List<List<String>>
@@ -33,6 +35,6 @@ data class Tables(val content: List<Table>) {
     fun get(name: String): Table = content.single { it[0][0] == name }
 
     fun getMappingTable(
-            name: String,
+        name: String,
     ): Map<String, String> = get(name).drop(1).associate { it[0] to it[1] }
 }
