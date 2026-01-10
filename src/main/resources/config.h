b336e56bf1107d65783dc1341ada94b8ecded0c8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-01-10 18:20:14 +0100

    optimize

--- src/main/resources/config.h
+++ src/main/resources/config.h
@@ -13,21 +13,6 @@
 
 ${triLayer}
 
-#define MK_3_SPEED
-#define MK_MOMENTARY_ACCEL
-#define MK_C_OFFSET_UNMOD   4
-#define MK_C_INTERVAL_UNMOD 16
-#define MK_C_OFFSET_0       2
-#define MK_C_INTERVAL_0     16
-#define MK_C_OFFSET_1       12
-#define MK_C_INTERVAL_1     16
-#define MK_C_OFFSET_2       24
-#define MK_C_INTERVAL_2     16
-#define MK_W_OFFSET_UNMOD   1
-#define MK_W_INTERVAL_UNMOD 40
-#define MK_W_OFFSET_0       1
-#define MK_W_INTERVAL_0     360
-#define MK_W_OFFSET_1       1
-#define MK_W_INTERVAL_1     120
-#define MK_W_OFFSET_2       1
-#define MK_W_INTERVAL_2     20
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
