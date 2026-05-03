b86bd1df65a4e6cb102af4dc041169fbb756801b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-05-03 12:27:43 +0200

    remove timeout config Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- src/main/resources/generated.c
+++ src/main/resources/generated.c
@@ -11,14 +11,6 @@ static bool magic_context_key_emitted = true;
 ${magicStringDecoder}
 ${magicCycleData}
 
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-    switch(index) {
-${timeouts}
-    default:
-        return COMBO_TERM;
-    }
-}
-
 // Two-variable tracking for real logical keys.
 // prev_keycode = the prior resolved key press (what adaptives and magics check against).
 // last_keycode = the most recent resolved key press (shifts to prev_keycode on the next keypress).
